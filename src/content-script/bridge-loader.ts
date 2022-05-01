81634c444448607179c907964ac28d94977674de
ZeekoZhu <vaezt@outlook.com>
2022-05-01 22:03:51 +0800

    chore: implement selectionchange event

--- src/content-script/bridge-loader.ts
+++ src/content-script/bridge-loader.ts
@@ -1,9 +1,5 @@
-import { content as log } from '../logger';
-
 export const loadBridge = () => {
   const script = document.createElement('script');
-  const scriptUrl = chrome.runtime.getURL('/figma-bridge.js');
-  log.debug('Loading bridge', script, scriptUrl);
-  script.src = scriptUrl;
+  script.src = chrome.runtime.getURL('/figma-bridge.js');
   document.head.appendChild(script);
 }
--- src/figma-bridge.ts
+++ src/figma-bridge.ts
@@ -1,3 +1,4 @@
+import { FigmaBridge } from './events';
 import { bridge as log } from './logger';
 
 log.debug('bridge init');
@@ -9,6 +10,23 @@ function notifyNodeSelected(nodeIdList: string[]) {
   }, '*');
 }
 
-document.addEventListener('click', () => {
-  notifyNodeSelected([]);
-});
+// @ts-ignore
+const figma = () => window['figma'];
+
+function waitForFigma() {
+  if (figma()) {
+    window.postMessage({
+      type: FigmaBridge.INITIALIZED,
+    });
+    // setup figma event listener
+    figma().on('selectionchange', () => {
+      const selection = figma().currentPage.selection;
+      const nodeIdList = selection.map((it: { id: string; }) => it.id);
+      notifyNodeSelected(nodeIdList);
+    });
+  } else {
+    setTimeout(waitForFigma, 100);
+  }
+}
+
+waitForFigma();
