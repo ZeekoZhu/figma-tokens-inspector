f22ce575fcaf05f63c8885075011d0e397fbfbb1
ZeekoZhu <vaezt@outlook.com>
2022-05-02 16:48:30 +0800

    wip: add basic popup frame

---
+++ src/popup/App.style.ts
@@ -0,0 +1,7 @@
+import { createStyles } from '@mantine/core';
+
+export const useAppStyles = createStyles(() => ({
+  root: {
+    width: '400px',
+  }
+}));
--- src/popup/App.tsx
+++ src/popup/App.tsx
@@ -1,42 +1,18 @@
-import { useState } from 'react'
+import { Container, MantineProvider, Tabs } from '@mantine/core';
+import { useAppStyles } from './App.style';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const { classes } = useAppStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <MantineProvider>
+      <Container className={classes.root}>
+        <Tabs>
+          <Tabs.Tab label="Inspect"></Tabs.Tab>
+          <Tabs.Tab label="Settings"></Tabs.Tab>
+        </Tabs>
+      </Container>
+    </MantineProvider>
   )
 }
 
-export default App
+export default App;
