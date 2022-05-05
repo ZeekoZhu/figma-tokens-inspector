c1fd72927e6ed98f9b35e12f70077307c63fb10c
ZeekoZhu <vaezt@outlook.com>
2022-05-05 17:35:02 +0800

    chore: get children node list from figma page

--- src/figma-bridge.ts
+++ src/figma-bridge.ts
@@ -1,6 +1,7 @@
 import { FigmaBridge } from './events';
 import { bridge as log } from './logger';
 
+
 log.debug('bridge init');
 
 function notifyNodeSelected(nodeIdList: string[]) {
@@ -13,6 +14,16 @@ function notifyNodeSelected(nodeIdList: string[]) {
 // @ts-ignore
 const figma = () => window['figma'];
 
+function* getSelfAndChildren(node: any): Generator<any, any, any> {
+  yield node;
+  if (!node.children) {
+    return;
+  }
+  for (const child of node.children) {
+    yield* getSelfAndChildren(child);
+  }
+}
+
 function waitForFigma() {
   if (figma()) {
     window.postMessage({
@@ -21,7 +32,11 @@ function waitForFigma() {
     // setup figma event listener
     figma().on('selectionchange', () => {
       const selection = figma().currentPage.selection;
-      const nodeIdList = selection.map((it: { id: string; }) => it.id);
+      log.debug('selectionchange', selection);
+      const nodeList = selection.flatMap((node: any) => {
+        return Array.from(getSelfAndChildren(node));
+      });
+      const nodeIdList = nodeList.map((it: { id: string; }) => it.id);
       notifyNodeSelected(nodeIdList);
     });
   } else {
